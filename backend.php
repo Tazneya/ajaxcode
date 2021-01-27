6e0295ea0fdebbbcfd40e2062b342c68230c22d1
Tazneya <safawthtasni@gmail.com>
2021-01-27 20:28:22 +0600

    first commit

---
+++ backend.php
@@ -0,0 +1,70 @@
+<?php
+
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db = "ajaxcrud";
+
+$conn = mysqli_connect($servername, $username, $password,$db);
+ extract($_POST);
+
+
+//data readkore table e how korbe
+ if(isset($_POST['readrecord'])){
+          $data = '<table class="table table-hover table-dark">
+          
+            <tr>
+              
+              <th >No</th>
+              <th >Email</th>
+              <th >pass</th>
+              <th >Mobile</th>
+              <th >Edit action</th>
+              <th >Delete action</th>
+              
+            </tr>';
+
+         $query = "SELECT * FROM crudtable";
+$tonni = mysqli_query($conn,$query);
+if(mysqli_num_rows($tonni)>0){
+    $number =1;
+    while($row = mysqli_fetch_array($tonni)){
+
+       $data.= '<tr>
+         
+          <td>.'$number'.</td>
+          <td>.'$row['email'].' </td>
+          <td>.'$row['pass'].' </td>
+          <td>.'$row['mobile'].' </td>
+          <td>.'$row['email'].' </td>
+          
+          <td>.<button onclick ="Getuserdetails('.$row['id'].')" class ="btn-warning">Edit</button> </td>
+          <td>.<button onclick ="Deleteuser('.$row['id'].')" class ="btn-warning">delete</button> </td>
+         
+        </tr>';
+        $number++;
+    }
+}
+$data.='</table>';
+echo $data;
+
+}
+          
+
+
+ //data insert korteche 
+ if(isset($_POST['insert']))
+ {
+     $email = $_POST['email'];
+     $pass = $_POST['pass'];
+     $mobile = $_POST['mobile'];
+     //file_put_contents('tazneya.txt',$email." ".$pass." ".$mobile);
+   $query = "INSERT INTO `crudtable`( `email`, `pass`, `mobile`) VALUES ('$email', '$pass', '$mobile' )";
+    mysqli_query($conn,$query);
+}
+
+ 
+ 
+ 
+ ?>
---
+++ indx.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+
+<div class ="container">
+  <h1 class='text-primary text-center text-uppercase'>AJAX CRUD OPERATION</h1>
+    <div class='d-flex justify-content-end'>
+      <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#myModal">Open modal</button>
+    </div>
+    <div>
+    <h1 class='text-warm'>
+    All Record 
+    </h1>
+    <div class='records_contant'>
+    </div>
+    
+    </div>
+    <!-- The Modal -->
+<div class="modal" id="myModal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+
+      <!-- Modal Header -->
+      <div class="modal-header">
+        <h4 class="modal-title">AJAX CRUD OPERATION</h4>
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+      </div>
+
+      <!-- Modal body -->
+   
+    
+      <div class="modal-body">
+      <div class="form-group">
+        <label >Email address</label>
+        <input type="email" class="form-control" id="Email" placeholder="Enter email">
+   
+      </div>
+      <div class="form-group">
+        <label>Password</label>
+        <input type="email" class="form-control" id="pass" placeholder="Enter pass">
+   
+      </div>
+      <div class="form-group">
+        <label for="exampleInputEmail1">mobile</label>
+        <input type="mobile" class="form-control" id="mobile" placeholder="Enter mobile">
+   
+      </div>
+      </div>
+   <!-- Modal footer -->
+   <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-dismiss="modal" onclick ='addRecord()'>Save</button>
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+      </div>
+  
+      </div>
+    
+      
+    </div>
+  </div>
+</div>
+  
+</div>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script type ="text/javascript">
+
+
+   function readRecord(){
+    var readrecord = "readrecord";
+    $.ajax({
+         url:"backend.php",
+         type:'POST',
+         data:{ readrecord : readrecord
+        },
+        success:function(data,status){
+           $('#records_contant').html(data);
+        }
+         });
+   }
+    function addRecord(){
+     
+      var email2 =$('#Email').val();
+      var pass2 =$('#pass').val();
+      var mobile2 =$('#mobile').val();
+      var insert = "insert";
+
+       $.ajax({
+         url:"backend.php",
+         type:'POST',
+         data:{ email : email2,
+          pass : pass2,
+          mobile : mobile2,
+          insert : insert
+          
+
+         },
+         success:function(data,status){
+           readRecords();
+         }
+       }
+         
+       )
+
+
+    }
+    </script>
+
+    </body>
+</html>
+
